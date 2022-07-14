ef627da7220c6d85fed5854865f2d3e4e031ba82
OUR0B0ROZ <jacobo.favela.irt@unipolidgo.edu.mx>
2022-07-14 17:18:44 -0500

    New updates for Heroku deployment

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,8 +26,7 @@ SECRET_KEY = 'django-insecure-hwphyv8eq@)1#@mkf(=0d)54&@pyg-dc&!7^1=d)*v829z!6(h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
